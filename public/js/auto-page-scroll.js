9363d460ff663164621e452ea97f0ce01ccbec1c
Vasanth Kumar P <vasanthk@infratab.in>
2013-12-04 23:23:57 +0530

    Getintership webpage - Updated

--- public/js/auto-page-scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(document).on('touchstart click', 'a', function(){
-    var currTarget = $(this).attr('data-loc');
-    $('html, body').stop().animate({
-      scrollTop: $($('#' + currTarget)).offset().top
-    }, 500);
-  });
-});
